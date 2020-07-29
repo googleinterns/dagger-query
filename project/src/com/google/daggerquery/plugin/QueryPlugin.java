f4e825c1898fdbe740136ed7425a2da7c3fce54c
margaritiko <margaritiko@gmail.com>
2020-07-29 19:51:56 +0300

    Added protocol buffer models

--- project/src/com/google/daggerquery/plugin/QueryPlugin.java
+++ project/src/com/google/daggerquery/plugin/QueryPlugin.java
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
  */
 
-package com.google.daggerquery;
+package com.google.daggerquery.plugin;
 
 import com.google.auto.service.AutoService;
 import dagger.model.BindingGraph;
