9da2012575c42b141fe623c3819855124ddae147
margaritiko <margaritiko@gmail.com>
2020-08-20 19:15:00 +0300

    Passing arguments without if statement

--- project/dagger-query.sh
+++ project/dagger-query.sh
@@ -1,7 +1,3 @@
 export PATH="$PATH:$HOME/bin"
 
-if [[ -z $3 ]]; then
-  bazel run --sandbox_debug --sandbox_block_path=/usr/local //src/com/google/daggerquery/executor:query_executor $1 $2
-else
-  bazel run --sandbox_debug --sandbox_block_path=/usr/local //src/com/google/daggerquery/executor:query_executor $1 $2 $3
-fi
+bazel run --sandbox_debug --sandbox_block_path=/usr/local //src/com/google/daggerquery/executor:query_executor $@
