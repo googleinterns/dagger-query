d2564dd8785de1b82de46e0912d8841e43298d40
margaritiko <margaritiko@gmail.com>
2020-09-14 10:29:50 +0300

    Moved the logic for sending requests and receiving responses to different functions

--- project/src/com/google/daggerqueryui/scripts/analyze_input_field_content.js
+++ project/src/com/google/daggerqueryui/scripts/analyze_input_field_content.js
@@ -59,6 +59,38 @@ $(function () {
   };
 });
 
+$(function () {
+  /**
+   * Sends a request to the server with specified query and gets a response.
+   * @param url an address with the correct path and request parameters in it
+   * @return {Promise<any>} a promise that contains a json with a graph on success
+   */
+  $.fn.getQueryResults = async function (url) {
+    let response = await fetch(url);
+
+    if (response.status === 200) {
+      return await response.json();
+    }
+
+    throw await response.text();
+  };
+
+  /**
+   * Processes a valid query by sending a request to the specified URL,
+   * receiving a response, and performing UI actions in response.
+   *
+   * @param url an address with the correct path and request parameters in it
+   */
+  $.fn.processQuery = async function (url) {
+    try {
+      const graph = await $(this).getQueryResults(url);
+      $(this).markInputFieldAsValid();
+    } catch (error) {
+      $(this).markInputFieldAsInvalid(error);
+    }
+  }
+});
+
 $("#query-input").on('keydown keyup change', function (event) {
   const query = $(this).val().trim().split(' ');
   const queryName = query[0];
@@ -73,19 +105,9 @@ $("#query-input").on('keydown keyup change', function (event) {
   queryNameElement.html(queryName).show();
 
   if (event.key === 'Enter' && $(this).validateParameters(query)) {
-    async function getQueryResults(url) {
-      let response = await fetch(url);
-
-      if (response.status === 200) {
-        return await response.json();
-      }
-
-      throw await response.text();
-    }
-
     const url = new URL(`http://localhost:4921/daggerquery/`);
     url.searchParams.append('query', query.join(' '));
 
-    const graph = getQueryResults(url).then($(this).markInputFieldAsValid).catch($(this).markInputFieldAsInvalid);
+    $(this).processQuery(url);
   }
 });
