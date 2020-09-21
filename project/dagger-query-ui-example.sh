7e1ce2bd2043ba2c8052fb4435cdc17d63a9323b
margaritiko <margaritiko@gmail.com>
2020-09-21 16:23:57 +0300

    Removes building an example target

--- project/dagger-query-ui-example.sh
+++ project/dagger-query-ui-example.sh
@@ -12,6 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-bazel run src/com/google/daggerquery/example:beach_example
 bazel run src/com/google/daggerquery:example_java_server
 
